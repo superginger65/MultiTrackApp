7a74be3cbb36d87eac6e347ee2a905ac4dba845c
superginger65 <superginger65@gmail.com>
2025-08-24 23:11:10 -0400

    Updated to have Year 2 and also Units that I have so far

---
+++ Year 1/TeacherStudentDuet10-Tripla/app.js
@@ -0,0 +1,190 @@
+
+let masterTrack = document.getElementById('master-audio');
+const timeline = document.getElementById('timeline');
+
+let trackToPlay = `Tracks/With Metronome/Combinations/Audio 11 Duet 10 Tripla Mix with Met.mp3`;
+let track1Mute = false;
+let track2Mute = false;
+let track3Mute = false;
+let track4Mute = false;
+
+  masterTrack.addEventListener('ended', endTracks);
+  masterTrack.addEventListener('timeupdate', function() {
+    timeline.value = this.currentTime;
+  });
+
+  function goBack() {
+      const params = new URLSearchParams(window.location.search);
+      const backPage = params.get('from');
+      if (backPage) {
+        location.href = '../' + backPage + '.html';
+      } else {
+        location.href = '../index.html';
+      }
+    }
+  
+  // Function to toggle mute on a track
+  function toggleMute(trackId) {
+    const track = document.getElementById(trackId);
+    track.muted = !track.muted;
+
+    const muteButton = document.getElementById(`mute-${trackId}`);
+    muteButton.textContent = track.muted ? '🔇' : '🔊';
+  }
+
+  function toggleTrack(trackId, shouldPlay) {
+
+    switch (trackId) {
+      case 1:
+        track1Mute = !track1Mute;
+        const muteButton1 = document.getElementById(`mute-track1`);
+        muteButton1.textContent = track1Mute ? '🔇' : '🔊';
+        break;
+      case 2:
+        track2Mute = !track2Mute;
+        const muteButton2 = document.getElementById(`mute-track2`);
+        muteButton2.textContent = track2Mute ? '🔇' : '🔊';
+        break;
+      case 3:
+        track3Mute = !track3Mute;
+        const muteButton3 = document.getElementById(`mute-track3`);
+        muteButton3.textContent = track3Mute ? '🔇' : '🔊';
+        break;
+      case 4:
+        track4Mute = !track4Mute;
+        const muteButton4 = document.getElementById(`mute-track4`);
+        muteButton4.textContent = track4Mute ? '🔇' : '🔊';
+        break;
+    }
+    if (!track1Mute && !track2Mute && !track3Mute && !track4Mute) {
+      trackToPlay = `Tracks/With Metronome/Combinations/Audio 11 Duet 10 Tripla Mix with Met.mp3`; 
+    } else if (track1Mute && !track2Mute && !track3Mute && !track4Mute) {
+      trackToPlay = `Tracks/With Metronome/Combinations/Audio 11 Duet 10 Tripla Teach Strum with Met.mp3`;
+    } else if (!track1Mute && track2Mute && !track3Mute && !track4Mute) {
+      trackToPlay = `Tracks/With Metronome/Combinations/Audio 11 Duet 10 Tripla Stud Strum with Met.mp3`;
+    } else if (!track1Mute && !track2Mute && track3Mute && !track4Mute) {
+      trackToPlay = `Tracks/With Metronome/Combinations/Audio 11 Duet 10 Tripla Stud Teach with Met.mp3`;
+    } else if (!track1Mute && !track2Mute && !track3Mute && track4Mute) {
+      trackToPlay = `Tracks/No Metronome/Combinations/Audio 11 Duet 10 Tripla Mix no Met.mp3`;
+    } else if (track1Mute && track2Mute && !track3Mute && !track4Mute) {
+      trackToPlay = `Tracks/With Metronome/Singles/Audio 11 Duet 10 Tripla Strum with Met.mp3`;
+    } else if (!track1Mute && track2Mute && track3Mute && !track4Mute) {
+      trackToPlay = `Tracks/With Metronome/Singles/Audio 11 Duet 10 Tripla Stud with Met.mp3`;
+    } else if (!track1Mute && !track2Mute && track3Mute && track4Mute) {
+      trackToPlay = `Tracks/No Metronome/Combinations/Audio 11 Duet 10 Tripla Stud Teach no Met.mp3`;
+    } else if (track1Mute && !track2Mute && track3Mute && !track4Mute) {
+      trackToPlay = `Tracks/With Metronome/Singles/Audio 11 Duet 10 Tripla Teach with Met.mp3`;
+    } else if (track1Mute && !track2Mute && !track3Mute && track4Mute) {
+      trackToPlay = `Tracks/No Metronome/Combinations/Audio 11 Duet 10 Tripla Teach Strum no Met.mp3`;
+    } else if (!track1Mute && track2Mute && !track3Mute && track4Mute) {
+      trackToPlay = `Tracks/No Metronome/Combinations/Audio 11 Duet 10 Tripla Stud Strum no Met.mp3`;
+    } else if (track1Mute && track2Mute && track3Mute && !track4Mute) {
+      trackToPlay = `Tracks/With Metronome/Singles/Audio 11 Duet 10 Tripla Met with Met.mp3`;
+    } else if (track1Mute && track2Mute && !track3Mute && track4Mute) {
+      trackToPlay = `Tracks/No Metronome/Singles/Audio 11 Duet 10 Tripla Strum no Met.mp3`;
+    } else if (track1Mute && !track2Mute && track3Mute && track4Mute) {
+      trackToPlay = `Tracks/No Metronome/Singles/Audio 11 Duet 10 Tripla Teach no Met.mp3`;
+    } else if (!track1Mute && track2Mute && track3Mute && track4Mute) {
+      trackToPlay = `Tracks/No Metronome/Singles/Audio 11 Duet 10 Tripla Stud no Met.mp3`;
+    } else if (track1Mute && track2Mute && track3Mute && track4Mute) {
+      toggleTrack(trackId, false);
+      return;
+    }
+    if (shouldPlay) {
+      let track = document.getElementById(`master-audio`);
+      let source = document.getElementById(`master-source`);
+      let wasPlaying = !track.paused;
+      track.pause();
+      let currentTime = track.currentTime;
+      source.src = trackToPlay;
+      track.load();
+      track.currentTime = currentTime;
+      if (wasPlaying) track.play();
+    }
+  }
+
+  // Function to change the playback speed based on the input BPM
+  function changeSpeed(bpm) {
+    const masterTrack = document.getElementById('master-audio');
+    
+    const baseBpm = 100;
+    const speed = bpm / baseBpm;
+    
+    // Adjust the playback speed
+    masterTrack.playbackRate = speed;
+  }
+  
+
+  // Function to play or pause all tracks in sync
+  function toggleMasterPlayPause() {
+    const masterTrack = document.getElementById('master-audio');
+    const masterButton = document.getElementById('master-play-pause');
+    
+    if (masterTrack.paused) {
+      masterTrack.play();
+      masterButton.textContent = '⏸︎';
+    }
+    else {
+      masterTrack.pause();
+      masterButton.textContent = '⏵︎';
+    }
+  }
+
+  function setTrackVolume(trackId, volume) {
+    const track = document.getElementById(trackId);
+    track.volume = volume;
+  }
+
+
+  function endTracks() {
+    const masterTrack = document.getElementById('master-audio');
+    const masterButton = document.getElementById('master-play-pause');
+    masterTrack.pause();
+    masterTrack.currentTime = 0;
+    masterButton.textContent = '⏵︎';
+  }
+
+
+  //Function to handle Restarting Tracks
+  function restartTracks() {
+    const masterTrack = document.getElementById('master-audio');
+    masterTrack.currentTime = 0;
+  }
+
+
+  //Function to handle moving the Time of the Tracks
+  function moveTracks(timeInSeconds) {
+    const masterTrack = document.getElementById('master-audio');
+    masterTrack.currentTime = masterTrack.currentTime + timeInSeconds;
+  }
+
+  //Function to handle moving the Time of the Tracks using the slider
+  function moveTracksSlider(timeInSeconds) {
+    const masterTrack = document.getElementById('master-audio');
+    masterTrack.currentTime = timeInSeconds;
+  }
+
+
+  function loopHandler() {
+    const loopStart = document.getElementById('loop-start');
+    const loopEnd = document.getElementById('loop-end');
+    if (this.currentTime >= loopEnd.value || this.currentTime < loopStart.value) {
+      this.currentTime = loopStart.value;
+      this.play();
+    }
+  }
+
+  function toggleLoop() {
+    const loopDiv = document.getElementById('loop-controls');
+    const masterTrack = document.getElementById('master-audio');
+
+    if (loopDiv.hidden === true) {
+
+      loopDiv.hidden = false;
+      masterTrack.addEventListener('timeupdate', loopHandler);
+    } else {
+      loopDiv.hidden = true;
+      masterTrack.removeEventListener('timeupdate', loopHandler);
+    }
+  }
+  
---
+++ Year 1/TeacherStudentDuet2-Play2/app.js
@@ -0,0 +1,199 @@
+
+let masterTrack = document.getElementById('master-audio');
+const timeline = document.getElementById('timeline');
+
+let trackToPlay = `Tracks/With Metronome/Combinations/Play 2 Mix All with met.mp3`;
+let track1Mute = false;
+let track2Mute = false;
+let track3Mute = false;
+let track4Mute = false;
+
+  masterTrack.addEventListener('ended', endTracks);
+  masterTrack.addEventListener('timeupdate', function() {
+    timeline.value = this.currentTime;
+  });
+
+  function goBack() {
+      const params = new URLSearchParams(window.location.search);
+      const backPage = params.get('from');
+      if (backPage) {
+        location.href = '../' + backPage + '.html';
+      } else {
+        location.href = '../index.html';
+      }
+    }
+  
+  // Function to toggle mute on a track
+  function toggleMute(trackId) {
+    const track = document.getElementById(trackId);
+    track.muted = !track.muted;
+
+    const muteButton = document.getElementById(`mute-${trackId}`);
+    muteButton.textContent = track.muted ? '🔇' : '🔊';
+  }
+
+  function toggleTrack(trackId, shouldPlay) {
+
+    switch (trackId) {
+      case 1:
+        track1Mute = !track1Mute;
+        const muteButton1 = document.getElementById(`mute-track1`);
+        muteButton1.textContent = track1Mute ? '🔇' : '🔊';
+        break;
+      case 2:
+        track2Mute = !track2Mute;
+        const muteButton2 = document.getElementById(`mute-track2`);
+        muteButton2.textContent = track2Mute ? '🔇' : '🔊';
+        break;
+      case 3:
+        track3Mute = !track3Mute;
+        const muteButton3 = document.getElementById(`mute-track3`);
+        muteButton3.textContent = track3Mute ? '🔇' : '🔊';
+        break;
+      case 4:
+        track4Mute = !track4Mute;
+        const muteButton4 = document.getElementById(`mute-track4`);
+        muteButton4.textContent = track4Mute ? '🔇' : '🔊';
+        break;
+    }
+    if (!track1Mute && !track2Mute && !track3Mute && !track4Mute) {
+      trackToPlay = `Tracks/With Metronome/Combinations/Play 2 Mix All with met.mp3`; 
+    } else if (track1Mute && !track2Mute && !track3Mute && !track4Mute) {
+      trackToPlay = `Tracks/With Metronome/Combinations/Play 2 Teach Strum with met.mp3`;
+    } else if (!track1Mute && track2Mute && !track3Mute && !track4Mute) {
+      trackToPlay = `Tracks/With Metronome/Combinations/Play 2 Stud Strum with met.mp3`;
+    } else if (!track1Mute && !track2Mute && track3Mute && !track4Mute) {
+      trackToPlay = `Tracks/With Metronome/Combinations/Play 2 Stud Teach with met.mp3`;
+    } else if (!track1Mute && !track2Mute && !track3Mute && track4Mute) {
+      trackToPlay = `Tracks/No Metronome/Combinations/Play 2 Mix All no met.mp3`;
+    } else if (track1Mute && track2Mute && !track3Mute && !track4Mute) {
+      trackToPlay = `Tracks/With Metronome/Singles/Play 2 Strum with met.mp3`;
+    } else if (!track1Mute && track2Mute && track3Mute && !track4Mute) {
+      trackToPlay = `Tracks/With Metronome/Singles/Play 2 Student with met.mp3`;
+    } else if (!track1Mute && !track2Mute && track3Mute && track4Mute) {
+      trackToPlay = `Tracks/No Metronome/Combinations/Play 2 Stud teach no met.mp3`;
+    } else if (track1Mute && !track2Mute && track3Mute && !track4Mute) {
+      trackToPlay = `Tracks/With Metronome/Singles/Play 2 Teacher with met.mp3`;
+    } else if (track1Mute && !track2Mute && !track3Mute && track4Mute) {
+      trackToPlay = `Tracks/No Metronome/Combinations/Play 2 Teach strum no met.mp3`;
+    } else if (!track1Mute && track2Mute && !track3Mute && track4Mute) {
+      trackToPlay = `Tracks/No Metronome/Combinations/Play 2 Stud strum no met.mp3`;
+    } else if (track1Mute && track2Mute && track3Mute && !track4Mute) {
+      trackToPlay = `Tracks/With Metronome/Singles/Play 2 Metronome.mp3`;
+    } else if (track1Mute && track2Mute && !track3Mute && track4Mute) {
+      trackToPlay = `Tracks/No Metronome/Singles/Play 2 strum.mp3`;
+    } else if (track1Mute && !track2Mute && track3Mute && track4Mute) {
+      trackToPlay = `Tracks/No Metronome/Singles/Play 2 Teacher.mp3`;
+    } else if (!track1Mute && track2Mute && track3Mute && track4Mute) {
+      trackToPlay = `Tracks/No Metronome/Singles/Play 2 Student.mp3`;
+    } else if (track1Mute && track2Mute && track3Mute && track4Mute) {
+      toggleTrack(trackId, false);
+      return;
+    }
+    if (shouldPlay) {
+      let track = document.getElementById(`master-audio`);
+      let source = document.getElementById(`master-source`);
+      let wasPlaying = !track.paused;
+      track.pause();
+      let currentTime = track.currentTime;
+      source.src = trackToPlay;
+      track.load();
+      track.currentTime = currentTime;
+      if (wasPlaying) track.play();
+    }
+  }
+
+  // Function to change the playback speed based on the input BPM
+  function changeSpeed(bpm) {
+    const masterTrack = document.getElementById('master-audio');
+    
+    const baseBpm = 100;
+    const speed = bpm / baseBpm;
+    
+    // Adjust the playback speed
+    masterTrack.playbackRate = speed;
+  }
+  
+
+  // Function to play or pause all tracks in sync
+  function toggleMasterPlayPause() {
+    const masterTrack = document.getElementById('master-audio');
+    const masterButton = document.getElementById('master-play-pause');
+    
+    if (masterTrack.paused) {
+      masterTrack.play();
+      masterButton.textContent = '⏸︎';
+    }
+    else {
+      masterTrack.pause();
+      masterButton.textContent = '⏵︎';
+    }
+  }
+
+  function setTrackVolume(trackId, volume) {
+    const track = document.getElementById(trackId);
+    track.volume = volume;
+  }
+
+
+  function endTracks() {
+    const masterTrack = document.getElementById('master-audio');
+    const masterButton = document.getElementById('master-play-pause');
+    masterTrack.pause();
+    masterTrack.currentTime = 0;
+    masterButton.textContent = '⏵︎';
+  }
+
+
+  //Function to handle Restarting Tracks
+  function restartTracks() {
+    const masterTrack = document.getElementById('master-audio');
+    masterTrack.currentTime = 0;
+  }
+
+
+  //Function to handle moving the Time of the Tracks
+  function moveTracks(timeInSeconds) {
+    const masterTrack = document.getElementById('master-audio');
+    masterTrack.currentTime = masterTrack.currentTime + timeInSeconds;
+  }
+
+  //Function to handle moving the Time of the Tracks using the slider
+  function moveTracksSlider(timeInSeconds) {
+    const masterTrack = document.getElementById('master-audio');
+    masterTrack.currentTime = timeInSeconds;
+  }
+
+
+  //Function to Reset the Speed to default
+
+  function resetSpeed() {
+    const slider = document.getElementById('speed');
+    slider.value = 100; // Reset the slider to 100
+    changeSpeed(100); // Reset the speed to default
+  }
+
+  function loopHandler() {
+    const loopStart = document.getElementById('loop-start');
+    const loopEnd = document.getElementById('loop-end');
+    if (this.currentTime >= loopEnd.value || this.currentTime < loopStart.value) {
+      this.currentTime = loopStart.value;
+      this.play();
+    }
+  }
+
+
+  function toggleLoop() {
+    const loopDiv = document.getElementById('loop-controls');
+    const masterTrack = document.getElementById('master-audio');
+
+    if (loopDiv.hidden === true) {
+
+      loopDiv.hidden = false;
+      masterTrack.addEventListener('timeupdate', loopHandler);
+    } else {
+      loopDiv.hidden = true;
+      masterTrack.removeEventListener('timeupdate', loopHandler);
+    }
+  }
+  
